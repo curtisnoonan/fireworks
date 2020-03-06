cfc66cb0f72eb3deb1e9ece95623a0abb92dd91f
Curtis Noonan <curtis.noonan@gmail.com>
2020-03-05 17:15:46 -0800

    Updates

---
+++ precache-manifest.dbbd713af3c0c24eab0d47cae07f1548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c15c801e20fc36cb2a18b08e23095e",
+    "url": "/fireworks/index.html"
+  },
+  {
+    "revision": "27a3f2fe7acfe5f333d2",
+    "url": "/fireworks/static/css/main.efd8e682.chunk.css"
+  },
+  {
+    "revision": "66ccc6ba444d384c4762",
+    "url": "/fireworks/static/js/2.51236393.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/fireworks/static/js/2.51236393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a3f2fe7acfe5f333d2",
+    "url": "/fireworks/static/js/main.b4b8bd14.chunk.js"
+  },
+  {
+    "revision": "b81ed055583f2be4f40d",
+    "url": "/fireworks/static/js/runtime-main.d4c68f91.js"
+  }
+]);
